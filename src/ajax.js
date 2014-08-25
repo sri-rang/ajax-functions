1e67308b5465b7000df110797aedfd3a631a1a36
Srirangan <srirangan@gmail.com>
2014-08-25 18:05:40 +0200

    IE8, IE9 and FormData Friendly

--- src/ajax.js
+++ src/ajax.js
@@ -4,40 +4,32 @@
     window.ajax = {};
 
     window.ajax.post = function (url, data, on_complete) {
-        var xhr;
+        var request,
+            is_cross_domain = url.indexOf(location.hostname) === -1;
 
-        if (typeof XMLHttpRequest !== "undefined") xhr = new XMLHttpRequest();
-        else {
-            var versions = [
-                "MSXML2.XmlHttp.5.0",
-                "MSXML2.XmlHttp.4.0",
-                "MSXML2.XmlHttp.3.0",
-                "MSXML2.XmlHttp.2.0",
-                "Microsoft.XmlHttp"
-            ];
-            for (var i = 0, len = versions.length; i < len; i++) {
-                try {
-                    xhr = new ActiveXObject(versions[i]);
-                    break;
-                }
-                catch (e) {}
-            }
-        }
-
-        xhr.onreadystatechange = ensure_readiness;
-
-        function ensure_readiness() {
-            if (xhr.readyState < 4) return;
-            if (xhr.status !== 200) return;
-            if (xhr.readyState === 4) {
-                var response = JSON.parse(xhr.responseText);
+        if (is_cross_domain && window.XDomainRequest) {
+            request = new XDomainRequest();
+            request.onprogress = function () {};
+            request.onload = function () {
+                var response = JSON.parse(request.responseText);
                 on_complete(response);
-            }
+            };
+        }
+        else {
+            request = new XMLHttpRequest();
+            request.onreadystatechange = function () {
+                if (request.readyState < 4) return;
+                if (request.status !== 200) return;
+                if (request.readyState === 4) {
+                    var response = JSON.parse(request.responseText);
+                    on_complete(response);
+                }
+            };
         }
 
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8;");
-        xhr.send(JSON.stringify(data));
+        request.open("POST", url, true);
+        if (!(data instanceof FormData)) data = JSON.stringify(data);
+        request.send(data);
     }
 
 })();
